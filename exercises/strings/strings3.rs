e0413dab4df6fd06997a7624a252689888d86947
Elonisme <elonisme@keemail.me>
2023-10-10 20:26:13 +0800

    update

--- exercises/strings/strings3.rs
+++ exercises/strings/strings3.rs
@@ -3,21 +3,26 @@
 // Execute `rustlings hint strings3` or use the `hint` watch subcommand for a
 // hint.
 
-// I AM NOT DONE
+// I AM DONE
 
 fn trim_me(input: &str) -> String {
     // TODO: Remove whitespace from both ends of a string!
-    ???
+    let s = input.trim();
+    s.to_string()
 }
 
 fn compose_me(input: &str) -> String {
     // TODO: Add " world!" to the string! There's multiple ways to do this!
-    ???
+    let s = String::from(input);
+    let world = String::from(" world!");
+    let output = s + &world;
+    output
 }
 
 fn replace_me(input: &str) -> String {
     // TODO: Replace "cars" in the string with "balloons"!
-    ???
+    let s = String::from(input);
+    s.replace("cars", "balloons")
 }
 
 #[cfg(test)]
